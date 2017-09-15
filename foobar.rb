e16ba45432d2516e9f154cc9671b971fa6bcd6f4
Reese Gyllenhammer <rgyllenhammer@gmail.com>
2017-09-15 00:20:09 -0700

    first commit for hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,23 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    unique = a.uniq
+
+    unique.map! do |el|
+      el = el.to_i
+      el +=2
+    end
+
+    unique.delete_if do |el|
+      el % 2 == 1 or el >= 10
+    end
+
+    unique.each do |el|
+      sum += el
+    end
+    
+    return sum
+
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,34 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
 
+  # longer method
+  # sum = a + b
+  # total = sum ** 2
+
+  # shorter method
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
 
+  # done out the long way
+
+  #sorted = a.sort
+  #add1 = sorted.map do |element|
+    #element = element + 1
+  #end
+
+  # shorter method
+  return a.sort.map do |el| el = el + 1 end
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + ' ' + last_name
 end
+
+
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
